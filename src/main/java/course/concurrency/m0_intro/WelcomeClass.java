cf09dd62df647ffdd54a0652ebbfa48e539c7d20
stepanenkostas87 <stepanenkostas87@yandex.ru>
2022-11-04 16:03:50 +0300

    Lesson 2-3. Code homework.

--- src/main/java/course/concurrency/m0_intro/WelcomeClass.java
+++ src/main/java/course/concurrency/m0_intro/WelcomeClass.java
@@ -1,10 +1,15 @@
 package course.concurrency.m0_intro;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class WelcomeClass {
 
     private final String message = "hello";
 
+
     public String getMessage() {
+        Executors.newSingleThreadExecutor();
         return message;
     }
 }
