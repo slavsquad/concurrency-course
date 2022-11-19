bcab2bc5b5b6327e96697b125b755fd486284d81
stepanenkostas87 <stepanenkostas87@yandex.ru>
2022-11-19 15:44:54 +0300

    Lesson 3. Collection 3-11 homework. RestaurantService Stat

--- src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
+++ src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
@@ -1,9 +1,9 @@
 package course.concurrency.m3_shared.collections;
 
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 public class RestaurantService {
 
@@ -13,7 +13,7 @@ public class RestaurantService {
         put("C", new Restaurant("C"));
     }};
 
-    private Object stat;
+    private Map<String, Integer> stat = new ConcurrentHashMap<>();
 
     public Restaurant getByName(String restaurantName) {
         addToStat(restaurantName);
@@ -21,11 +21,13 @@ public class RestaurantService {
     }
 
     public void addToStat(String restaurantName) {
-        // your code
+        stat.merge(restaurantName, 1, Integer::sum);// your code
     }
 
     public Set<String> printStat() {
         // your code
-        return new HashSet<>();
+        return stat.entrySet().stream()
+                .map(v -> v.getKey() + " - " + v.getValue())
+                .collect(Collectors.toSet());
     }
 }
